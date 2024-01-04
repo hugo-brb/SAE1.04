385dfe7e54020156f10d3b5d8e277ea0b02d3699
Hugo BARBIERI <hugobarbieri38@gmail.com>
2024-01-04 15:08:02 +0000

    modif

--- Etape3/new_peuplement.sql
+++ Etape3/new_peuplement.sql
@@ -1,8 +1,8 @@
-\copy PORT FROM '../donnes/PORTS.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
-\copy PASSENGER FROM '../donnes/Patch_Passengers.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
-\copy OCCUPATION FROM '../donnes/OCCUPATION.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
-\copy SERVICE FROM '../donnes/SERVICE.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
-\copy CATEGORY FROM '../donnes/CATEGORY.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
-\copy LIFEBOAT FROM '../donnes/LIFEBOAT.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
-\copy RECOVERY FROM '../donnes/RECOVERY.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
-\copy RESCUE FROM '../donnes/RESCUE.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
+\copy PORT FROM '/workspaces/SAE1.04/donnes/PORTS.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
+\copy PASSENGER FROM '/workspaces/SAE1.04/donnes/Patch_Passengers.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
+\copy OCCUPATION FROM '/workspaces/SAE1.04/donnes/OCCUPATION.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
+\copy SERVICE FROM '/workspaces/SAE1.04/donnes/SERVICE.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
+\copy CATEGORY FROM '/workspaces/SAE1.04/donnes/CATEGORY.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
+\copy LIFEBOAT FROM '/workspaces/SAE1.04/donnes/LIFEBOAT.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
+\copy RECOVERY FROM '/workspaces/SAE1.04/donnes/RECOVERY.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
+\copy RESCUE FROM '/workspaces/SAE1.04/donnes/RESCUE.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
