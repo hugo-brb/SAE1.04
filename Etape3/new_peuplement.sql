5f79ba244b2ad300a633ee96dd32a236a44367c5
Hugo BARBIERI <hugobarbieri38@gmail.com>
2024-01-04 15:11:39 +0000

    renamed

--- Etape3/new_peuplement.sql
+++ Etape3/new_peuplement.sql
@@ -1,8 +1,8 @@
-\copy PORT FROM '/workspaces/SAE1.04/donnes/PORTS.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
-\copy PASSENGER FROM '/workspaces/SAE1.04/donnes/Patch_Passengers.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
-\copy OCCUPATION FROM '/workspaces/SAE1.04/donnes/OCCUPATION.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
-\copy SERVICE FROM '/workspaces/SAE1.04/donnes/SERVICE.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
-\copy CATEGORY FROM '/workspaces/SAE1.04/donnes/CATEGORY.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
-\copy LIFEBOAT FROM '/workspaces/SAE1.04/donnes/LIFEBOAT.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
-\copy RECOVERY FROM '/workspaces/SAE1.04/donnes/RECOVERY.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
-\copy RESCUE FROM '/workspaces/SAE1.04/donnes/RESCUE.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
+\copy PORT FROM '/workspaces/SAE1.04/donnees/PORTS.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
+\copy PASSENGER FROM '/workspaces/SAE1.04/donnees/Patch_Passengers.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
+\copy OCCUPATION FROM '/workspaces/SAE1.04/donnees/OCCUPATION.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
+\copy SERVICE FROM '/workspaces/SAE1.04/donnees/SERVICE.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
+\copy CATEGORY FROM '/workspaces/SAE1.04/donnees/CATEGORY.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
+\copy LIFEBOAT FROM '/workspaces/SAE1.04/donnees/LIFEBOAT.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
+\copy RECOVERY FROM '/workspaces/SAE1.04/donnees/RECOVERY.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
+\copy RESCUE FROM '/workspaces/SAE1.04/donnees/RESCUE.csv' WITH (DELIMITER ';', format CSV, HEADER, ENCODING 'UTF8');
