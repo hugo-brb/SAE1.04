d469de4f78ae6e9f84f98b0e0daabb6650afefd5
Hugo BARBIERI <hugobarbieri38@gmail.com>
2024-01-09 21:43:56 +0000

    modified

--- Etape3/etape3.sql
+++ Etape3/etape3.sql
@@ -1,86 +1,19 @@
-/* Relation PORT dérivée de l'entité PORT du SEA -App. R0 */
-CREATE TABLE PORT (
-  PortId char(1) primary key CHECK (PortId IN ('C','Q','S')),
-  PortName varchar NOT NULL,
-  Country varchar NOT NULL
-);
-/*
-Relation PASSENGER dérivée de l'entité PASSENGER du SEA - App. R0
-complétée par :
-  - l'identifiant de l'entité CLASS (non conservée) - App. R1
-  - l'identifiant de l'entité PORT (conservée) - App. R1
-*/
-CREATE TABLE PASSENGER (
-  PassengerId int primary key,
-  Name varchar NOT NULL,
-  Sex varchar NOT NULL CHECK (Sex IN ('male','female')),
-  Age int CHECK(Age >= 0),
-  Survived int NOT NULL CHECK (Survived IN (0,1)),
-  PClass int NOT NULL CHECK (PClass BETWEEN 1 and 3),
-  PortId char(1) references PORT(PortId)
-);
-/* Relation OCCUPATION dérivée de l'assoication Occupation - App. R3 */
-CREATE TABLE OCCUPATION (
-  PassengerId int References PASSENGER(PassengerId),
-  CabinCode varchar,
-  Primary key (PassengerId, CabinCode)
-);
-/* Relation SERVICE dérivée de l'association service - App. R2 */
-CREATE TABLE SERVICE (
-  PassengerId_Dom int primary key references PASSENGER(PassengerId),
-  PassengerId_Emp int NOT NULL,
-  foreign key (PassengerId_Emp) references PASSENGER(PassengerId),
-  Role varchar NOT NULL,
-  CHECK (PassengerId_Dom != PassengerID_Emp)
-);
-/* REALTION CATEGORY dérivée de l'entité CATEGORY du SEA - App. R0 */
-CREATE TABLE CATEGORY (
-  LifeBoatCat varchar primary key CHECK (LifeBoatCat IN ('standard','secours','radeau')),
-  Structure varchar NOT NULL CHECK (Structure IN ('bois', 'bois et toile')),
-  Places int NOT NULL CHECK (Places > 0)
-);
-/*
-Relation LIFEBOAT dérivée de l'entite LIFEBOAT du SEA - APP. R0
-complétée par :
-  - l'identifiant de l'entité CATEGORY (conservée) - App. R1
-  - L'identifiant de l'entité TIME (non consevée) - App. R1
-*/
-CREATE TABLE LIFEBOAT (
-  LifeBoatId varchar primary key,
-  LifeBoatCat varchar NOT NULL references CATEGORY(LifeBoatCat),
-  Side varchar NOT NULL CHECK (Side IN ('babord','tribord')),
-  Position varchar NOT NULL CHECK (Position IN ('avant','arriere')),
-  Location varchar NOT NULL DEFAULT 'Pont', -- pour dire
-  Launching_Time Time NOT NULL
-);
-/*
-Relation RECOVERY dérivée de l'assocation recovery (non conservée) - App. R2
-*/
-CREATE TABLE RECOVERY (
-  LifeBoatId varchar references LIFEBOAT(LifeBoatId) primary key,
-  Recovery_time time NOT NULL
-);
-/* Relation RESCUE dériveé de lassociation rescue - App. R2 */
-CREATE TABLE RESCUE (
-  PassengerID int references PASSENGER(PassengerId) primary key,
-  LifeBoatId varchar NOT NULL references LIFEBOAT(LifeboatId)
-);
+/* PARTIE A*/
+    -- A1 --
 
--- A1 --
-
--- Nombre de survivants, respectivement de victimes parmi les passagers, selon leur classe (une requête par classe)
--- 1er classe
+-- a) Nombre de survivants, respectivement de victimes parmi les passagers, selon leur classe (une requête par classe)
+        -- 1ière classe
 SELECT
     (SELECT COUNT(*) 
     FROM PASSENGER 
     GROUP BY PClass, Survived
-    HAVING PClass = 1 and Survived = 0) as Nombre_de_1ere_classes_mort,
+    HAVING PClass = 1 and Survived = 0) as Nombre_de_1ere_classes_décédés,
     (SELECT COUNT(*) 
     FROM PASSENGER 
     GROUP BY PClass, Survived
     HAVING PClass = 1 and Survived = 1) as Nombre_de_1ere_classes_survivants;
-    --Autre Possibilité
-/*SELECT 
+/*Autre Possibilité
+SELECT 
     CASE Survived
         WHEN 0 THEN 'mort'
         WHEN 1 THEN 'survivant'
@@ -91,7 +24,7 @@ GROUP BY PClass, Survived
 HAVING PClass = 1
 ORDER BY Survived;*/
 
--- 2ième classe
+        -- 2ième classe
 SELECT
     (SELECT COUNT(*) 
     FROM PASSENGER 
@@ -102,7 +35,7 @@ SELECT
     GROUP BY PClass, Survived
     HAVING PClass = 2 and Survived = 1) as Nombre_de_2ieme_classes_survivants;
     
--- 3ième classe
+        -- 3ième classe
 SELECT
     (SELECT COUNT(*) 
     FROM PASSENGER 
@@ -113,7 +46,9 @@ SELECT
     GROUP BY PClass, Survived
     HAVING PClass = 3 and Survived = 1) as Nombre_de_3ieme_classes_survivants;
 
--- Nombre de survivants, respectivement de victimes parmi les passagers, selon leur catégorie (enfant, femme ou homme – une requête par catégorie) 
+
+-- b) Nombre de survivants, respectivement de victimes parmi les passagers, selon leur catégorie (enfant, femme ou homme – une requête par catégorie) 
+        -- Enfants
 SELECT
     (SELECT COUNT(*) 
     FROM PASSENGER 
@@ -122,14 +57,16 @@ SELECT
     FROM PASSENGER 
     WHERE Age < 12 and Survived = 1) as Nombre_enfant_survivants;
 
+        -- Femmes
 SELECT
     (SELECT COUNT(*) 
     FROM PASSENGER 
     WHERE Sex = 'female' and age>=12 and Survived = 0) as Nombre_femme_mort,
     (SELECT COUNT(*) 
     FROM PASSENGER 
-    WHERE Sex = 'female' and age>=12 and  Survived = 1) as Nombre_femme_survivants;
-    
+    WHERE Sex = 'female' and age>=12 and Survived = 1) as Nombre_femme_survivants;
+
+        -- Hommes  
 SELECT
     (SELECT COUNT(*) 
     FROM PASSENGER 
@@ -137,10 +74,11 @@ SELECT
     (SELECT COUNT(*) 
     FROM PASSENGER 
     WHERE Sex = 'male' and age>=12 and Survived = 1) as Nombre_homme_survivants;
-    -- Autre possibilité
-/*SELECT 
+
+/*Autre possibilité
+SELECT 
     CASE 
-        WHEN Age < 18 THEN 'Enfant'
+        WHEN Age < 12 THEN 'Enfant'
         WHEN Sex = 'female' THEN 'Femme'
         ELSE 'Homme'
     END AS Categorie,
@@ -149,35 +87,59 @@ FROM PASSENGER
 WHERE Survived = 1
 GROUP BY Categorie;*/
 
+        -- Comparaison survivants
+SELECT COUNT(*) as total_survivants, 
+(SELECT COUNT(*) 
+FROM PASSENGER
+WHERE Age < 12 and Survived = 1) as total_survivants_enfants,
+(SELECT COUNT(*) 
+FROM PASSENGER
+WHERE Sex = 'female' and Age >= 12 and Survived = 1) as total_survivants_femmes,
+(SELECT COUNT(*) 
+FROM PASSENGER
+WHERE Sex = 'male' and Age >= 12 and Survived = 1) as total_survivants_hommes
+FROM PASSENGER
+WHERE Survived = 1;
 
--- PARTIE A3 --------------------------------------------------------------------------------------
+        -- Comparaison décès
+SELECT COUNT(*) as total_décès, 
+(SELECT COUNT(*) 
+FROM PASSENGER
+WHERE Age < 12 and Survived = 0) as total_décès_enfants,
+(SELECT COUNT(*) 
+FROM PASSENGER
+WHERE Sex = 'female' and Age >= 12 and Survived = 0) as total_décès_femmes,
+(SELECT COUNT(*) 
+FROM PASSENGER
+WHERE Sex = 'male' and Age >= 12 and Survived = 0) as total_décès_hommes
+FROM PASSENGER
+WHERE Survived = 0;
 
--- taux de survivants par classe, toutes catégories confondues (enfants, femmes ou hommes)
 
+    -- A3 --
+
+-- a) Taux de survivants par classe, toutes catégories confondues (enfants, femmes ou hommes)
 SELECT PClass as classe, round(avg(survived*100),2) as taux_survivants
 FROM PASSENGER
 GROUP BY PClass
 ORDER BY PClass;
 
 
--- Taux de survivants par classe dans la catégorie enfants
-
+-- b) Taux de survivants par classe dans la catégorie enfants
 SELECT PClass as classe, round(avg(survived*100),2) as taux_survivants
 FROM PASSENGER
 WHERE age<12
 GROUP BY PClass
 ORDER BY PClass;
 
--- Taux de survivants par classe dans la catégorie femme
-
+-- c) Taux de survivants par classe dans la catégorie femme
 SELECT PClass as classe, round(avg(survived*100),2) as taux_survivants
 FROM PASSENGER
 WHERE Sex = 'female' and age>=12
 GROUP BY PClass
 ORDER BY PClass;
 
--- Taux de survivants par classe dans la catégorie hommes
-
+-- d) Taux de survivants par classe dans la catégorie hommes
 SELECT PClass as classe, round(avg(survived*100),2) as taux_survivants
 FROM PASSENGER
 WHERE Sex = 'male' and age>=12
@@ -185,10 +147,9 @@ GROUP BY PClass
 ORDER BY PClass;
 
 
--- A4 --
--- (a) Nombre total d'enfants et nombre d'enfants rescapés
+    -- A4 --
 
-
+-- a) Nombre total d'enfants et nombre d'enfants rescapés
 SELECT
     (SELECT COUNT(*)
     FROM PASSENGER
@@ -198,17 +159,14 @@ SELECT
     WHERE p.PassengerID = r.PassengerID
     and p.Age < 12) as nb_enfants_rescapes;
 
--- (b) Nombre d'enfants qui ont survécu parmi les enfants qui ont été rescapés
-
-
+-- b) Nombre d'enfants qui ont survécu parmi les enfants qui ont été rescapés
 SELECT COUNT(*) as nb_enfants_surv_dans_rescapes
 FROM RESCUE r, PASSENGER p
 WHERE r.PassengerID = p.PassengerID
 and Survived = 1
 and p.Age < 12;
 
--- (c) Pour chaque classe de passagers : nombre d'enfants qui ont survécu parmi les enfants rescapés
-
+-- c) Pour chaque classe de passagers : nombre d'enfants qui ont survécu parmi les enfants rescapés
 SELECT
     (SELECT COUNT(*)
     FROM RESCUE r, PASSENGER p
@@ -229,15 +187,12 @@ SELECT
     and PClass = 2
     and p.Age < 12) as nb_enfts_class3_surv_dans_rescapes;
 
-
--- (d) Taux de rescapés parmi les passagers
-
+-- d) Taux de rescapés parmi les passagers
 SELECT round(100 *
     (SELECT count(*) FROM RESCUE) / 
     (SELECT COUNT(*) FROM PASSENGER),2) as taux_rescapes;
 
--- (e) Nombre de rescapés par catégorie de passager (enfant, femme ou homme)
-
+-- e) Nombre de rescapés par catégorie de passager (enfant, femme ou homme)
 SELECT (SELECT COUNT(*)
     FROM RESCUE r, PASSENGER p
     WHERE p.PassengerID = r.PassengerID
@@ -251,9 +206,7 @@ SELECT (SELECT COUNT(*)
     WHERE p.PassengerID = r.PassengerID
     and Sex='male' and age>=12) as nb_hommes_rescapes;
 
-
-
--- (f) Nombre de survivants par catégorie de rescapés (enfant, femme ou homme)
+-- f) Nombre de survivants par catégorie de rescapés (enfant, femme ou homme)
 
 SELECT (SELECT COUNT(*)
     FROM RESCUE r, PASSENGER p
@@ -271,8 +224,7 @@ SELECT (SELECT COUNT(*)
     and Survived = 1
     and Sex='male' and age>=12) as nb_hommes_surv_dans_rescapes;
 
--- (g) Nombre total de rescapés et taux de survivants par embarcation - résultat ordonné sur le code de l'embarcation
-
+-- g) Nombre total de rescapés et taux de survivants par embarcation - résultat ordonné sur le code de l'embarcation
 SELECT LifeBoatId, count(LifeBoatId) as nb_rescapes, 100*(SELECT COUNT(PassengerID) 
       FROM PASSENGER 
       WHERE PassengerID IN (SELECT PassengerID 
@@ -283,9 +235,7 @@ FROM RESCUE r1
 GROUP BY LifeBoatId
 ORDER BY LifeboatId;
 
--- (h) Pour chaque classe de passager, nombre d'enfants, nombre de femmes et nombre d'hommes qui ont survécu parmi les rescapés
-
-
+-- h) Pour chaque classe de passager, nombre d'enfants, nombre de femmes et nombre d'hommes qui ont survécu parmi les rescapés
 SELECT PClass as classe, (SELECT COUNT(*)
     FROM RESCUE r, PASSENGER p
     WHERE r.PassengerID = p.PassengerID
@@ -310,17 +260,53 @@ ORDER BY PClass;
 
 
 
--- B --
--- (a) On verifie si les domestiques ont été rescapés
-SELECT COUNT(*) AS Total_Domestics,
-     COUNT(CASE WHEN Survived = 1 THEN 1 END) AS Rescued_Domestics
-FROM PASSENGER
-WHERE Occupation = 'Domestic';
+/*Partie B*/
+
+-- 1) Combien de domestiques ont survécu
+SELECT 
+    (SELECT COUNT(*) 
+    FROM PASSENGER
+    WHERE passengerid IN (SELECT PassengerId_Dom FROM service)
+    AND survived = 1) as nb_domestiques_survivant,
+    (SELECT COUNT(*) 
+    FROM PASSENGER
+    WHERE passengerid IN (SELECT PassengerId_Dom FROM service)
+    AND survived = 0) as nb_domestiques_mort;
+
+    -- Quid des employeurs des survivants ?
+SELECT COUNT(*) as nb_employeurs_rescapes
+FROM PASSENGER p, service s
+WHERE p.passengerid = s.PassengerID_Emp
+AND p.survived = 1
+AND s.PassengerID_Dom IN (SELECT PassengerID FROM RESCUE);
+
+-- 2) Influence de l'emplacement des embarcations de sauvetage (side et/ou position) sur le taux de survie des passagers qui y ont pris place ?
+SELECT l.LifeBoatId, l.side, l.position, count(r1.LifeBoatId) as nb_rescapes, 100*(SELECT COUNT(PassengerID) 
+      FROM PASSENGER 
+      WHERE PassengerID IN (SELECT PassengerID 
+            FROM RESCUE r2 
+            WHERE r2.LifeBoatId = l.LifeBoatId)
+      and survived=1 )/count(r1.LifeBoatId) as taux_survivants
+FROM RESCUE r1, LIFEBOAT l
+WHERE r1.LifeBoatId = l.LifeBoatId
+GROUP BY l.LifeBoatId
+ORDER BY l.LifeboatId;
+
+-- 3) Influence de l'heure de mise à l'eau des embarcations de sauvetage sur le taux de survie des passagers qui y ont pris place (ou encore, influence de l'heure de récupération de ces embarcations par le Carpathia)?
+SELECT l.LifeBoatId, launching_time, (SELECT recovery_time 
+FROM RECOVERY WHERE LifeBoatId = l.LifeBoatId), 100*(SELECT count(*) FROM RESCUE
+WHERE LifeBoatId = l.LifeBoatId)/count(r.LifeBoatId) as taux_survivants
+FROM LIFEBOAT l, RESCUE r
+WHERE l.LifeBoatId = r.LifeBoatId
+GROUP BY l.LifeBoatId
+ORDER BY l.LifeboatId;
 
 
--- Q4 : Taux de survie par tranche d'âge parmi les passagers ayant au moins 12 ans lors du naufrage :
--- Tranches : 12-16 ans, 17-29 ans, 30-39 ans, 40-49 ans, 50-59 ans, 60-69 ans, et +70 ans
 
+
+
+-- 4) Taux de survie par tranche d'âge parmi les passagers ayant au moins 12 ans lors du naufrage :
+  -- Tranches : 12-16 ans, 17-29 ans, 30-39 ans, 40-49 ans, 50-59 ans, 60-69 ans, et +70 ans
 SELECT (SELECT round(avg(survived*100),2)
     FROM PASSENGER
     WHERE age>=12
@@ -349,18 +335,14 @@ SELECT (SELECT round(avg(survived*100),2)
     FROM PASSENGER
     WHERE age>=70) as taux_plus_70ans;
 
-
-
---Q5 Combien de passagers supplémentaires auraient pu être rescapés (et peut-être survivre) si le taux
--- maximum de remplissage des embarcations de sauvetage avait été respecté ?
+-- 5) Combien de passagers supplémentaires auraient pu être rescapés (et peut-être survivre) si le taux
+  -- maximum de remplissage des embarcations de sauvetage avait été respecté ?
 
 -- selon les données du site https://titanic-1912.fr/ menu : "Les canots de sauvetage" => "Bilan du sauvetage",
 -- le nombre moyen de membre d'équipage est ±6 par canot. Je vais donc ajouter artificielement 6 personnes
 -- par canot pour avoir un nombre approximatif de places libres.
 
-
 -- Première requête qui donne le nombre de rescapés, de places, et de places libres pour chaque canot :
-
 SELECT r.LifeBoatId, count(r.LifeBoatId)+6 as nb_rescapes, places as places, places-6-count(r.LifeBoatId) as nb_places_libres
 FROM RESCUE r, CATEGORY c, LIFEBOAT l
 WHERE r.LifeBoatId = l.LifeBoatId
@@ -368,10 +350,7 @@ and l.LifeBoatCat = c.LifeBoatCat
 GROUP BY r.LifeBoatId, places
 ORDER BY r.LifeboatId;
 
-
 -- Requête qui donne le nombre de rescapés, de places, et de places libres pour tous les canots :
-
-
 SELECT SUM(nb_rescapes) as total_rescapes, SUM(places) as nb_places_max, SUM(nb_places_libres) as total_places_libres
 FROM (
     SELECT count(r.LifeBoatId)+6 as nb_rescapes, places as places, places-6-count(r.LifeBoatId) as nb_places_libres
@@ -380,8 +359,3 @@ FROM (
     and l.LifeBoatCat = c.LifeBoatCat
     GROUP BY r.LifeBoatId, places
     ORDER BY r.LifeboatId) as somme;
-
-
-
-
-
